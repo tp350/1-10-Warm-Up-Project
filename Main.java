a3bb73a136c60ebcea80daeae5ce4758f9a2d175
tp350 <fp7623@pleasantonusd.net>
2025-01-10 22:52:25 +0000

    done

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
-//test project
+//my project
+public class Main {
+    public static void main (String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i; //sum here
+            }
+        }
+        System.out.println ("Sum of multiples of 3 or 5 below 1000 is: " + sum);
+    }
+}
